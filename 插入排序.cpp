9f64b80980930ad58af31d616744a3c01ed40b15
tobybo <1175801693@qq.com>
2020-05-26 08:17:02 +0800

    bugfix

--- 插入排序.cpp
+++ 插入排序.cpp
@@ -30,7 +30,7 @@ void insert_sort(int* arr, int len){
     int temp;
     for (int use_pos = 1;use_pos<len;use_pos++){
         temp = arr[use_pos]; //待插入的数
-        for(int move_pos = use_pos;move_pos > 0 && arr[move_pos - 1] ;move_pos++){
+        for(int move_pos = use_pos;move_pos > 0 && arr[move_pos - 1] > temp;move_pos--)
 			arr[move_pos] = arr[move_pos - 1]; //将比待插入数大的数都向后移动一位
 			                                   //不用担心覆盖值 因为最后一位保存的是待插入的数 已经被存到temp里
         arr[move_pos] = temp; //move_pos永远指向的是可以存放temp的位置
